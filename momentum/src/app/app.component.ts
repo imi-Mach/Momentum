eff461befeda20e46450a249e228f4595db04e2a
Maximillian Machado <maximillian.k.machado@gmail.com>
2022-02-19 18:09:25 -0500

    Loaded in ag-grid

--- momentum/src/app/app.component.ts
+++ momentum/src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'momentum';
+  
+  columnDefs = [
+    { headerName: "Make", field: "make" },
+    { headerName: "Model", field: "model" },
+    { headerName: "Price", field: "price" },
+  ];
+
+  rowData = [
+    { make: 'ex1', model: 'aaa', price: 1000},
+    { make: 'ex2', model: 'bbb', price: 2000}
+  ];
 }
