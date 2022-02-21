8f9fb1f6a15955552d222403d943157a6b5eb439
Maximillian Machado <maximillian.k.machado@gmail.com>
2022-02-21 11:35:34 -0500

    added header and fixed toolbar

--- momentum/src/app/app.module.ts
+++ momentum/src/app/app.module.ts
@@ -15,12 +15,14 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import {MatExpansionModule} from '@angular/material/expansion';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GridComponent,
     SlidebarComponent,
+    HeaderComponent,
 
   ],
   imports: [
